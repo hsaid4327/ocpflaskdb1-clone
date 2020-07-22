401eac35ccd7fb6251f4a0e7007fb273f35d1f79
hmcbbs <hmcbbs@gmail.com>
2020-07-22 12:24:45 -0700

    Add files via upload

---
+++ 1.py
@@ -0,0 +1,46 @@
+#!/usr/bin/python3
+#
+#   Connect to Azure DB
+#   Run SQL query
+#   Display some output to prove it
+#   password for dbpass need to be set
+
+import pandas as pd
+import pyodbc
+from flask import Flask, redirect, url_for
+
+dbserv='uswsql2.azvms.com,51005'
+dbname='mfgnetdev'
+dbuser='mfgnetuser'
+dbpass=''
+conn = pyodbc.connect('DSN=TNSDB;SERVER='+ dbserv +';DATABASE='+ dbname +';UID='+ dbuser +';PWD='+ dbpass)
+site_code = 'fxc'
+sql_site="select site_name from sites where site_code = '" +  site_code + "'"
+
+app = Flask(__name__)
+
+@app.route("/site/")
+def site():
+    return f"Hello {site_code}!"
+
+@app.route("/fxc/")
+def fxc():
+    df_site=pd.read_sql(sql_site,conn)
+    return f"Hello {df_site}!"    
+
+@app.route("/home/")
+def home():
+    return "Hello! this is the main page <H1> HELLO </H1>"
+
+
+@app.route("/<name>")
+def user(name):
+    return f"Hello {name}!"
+
+@app.route("/admin")
+def admin():
+    return redirect(url_for("home")
+    
+    )
+if __name__ == "__main__":
+    app.run()
